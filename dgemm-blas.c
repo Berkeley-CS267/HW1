654f72cac1df267644f357db6559f2e25b659782
Alex Reinking <alex.reinking@gmail.com>
2020-01-18 14:53:30 -0800

    Initial commit.

---
+++ dgemm-blas.c
@@ -0,0 +1,26 @@
+extern void dgemm_(char*, char*, int*, int*, int*, double*,
+                   double*, int*, double*, int*, double*, double*, int*); 
+
+const char* dgemm_desc = "Reference dgemm.";
+
+/*
+ * This routine performs a dgemm operation
+ *  C := C + A * B
+ * where A, B, and C are lda-by-lda matrices stored in column-major format.
+ * On exit, A and B maintain their input values.    
+ * This function wraps a call to the BLAS-3 routine DGEMM,
+ * via the standard FORTRAN interface - hence the reference semantics.
+ */
+void square_dgemm(int N, double* A, double* B, double* C)
+{
+  char TRANSA = 'N';
+  char TRANSB = 'N';
+  int M = N;
+  int K = N;
+  double ALPHA = 1.;
+  double BETA = 1.;
+  int LDA = N;
+  int LDB = N;
+  int LDC = N;
+  dgemm_(&TRANSA, &TRANSB, &M, &N, &K, &ALPHA, A, &LDA, B, &LDB, &BETA, C, &LDC);
+}
